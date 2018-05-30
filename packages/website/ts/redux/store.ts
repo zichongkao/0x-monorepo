a88473a1cb7a17b03c8bc51caa8e3f7cf88406d8
fragosti <francesco.agosti93@gmail.com>
2018-05-30 11:21:11 -0700

    Add MS to ONE_SECOND var

--- packages/website/ts/redux/store.ts
+++ packages/website/ts/redux/store.ts
@@ -4,7 +4,7 @@ import { devToolsEnhancer } from 'redux-devtools-extension/developmentOnly';
 import { stateStorage } from 'ts/local_storage/state_storage';
 import { reducer, State } from 'ts/redux/reducer';
 
-const ONE_SECOND = 1000;
+const ONE_SECOND_MS = 1000;
 
 export const store: ReduxStore<State> = createStore(
     reducer,
@@ -17,5 +17,5 @@ store.subscribe(
         stateStorage.saveState({
             hasPortalOnboardingBeenSeen: store.getState().hasPortalOnboardingBeenSeen,
         });
-    }, ONE_SECOND),
+    }, ONE_SECOND_MS),
 );
